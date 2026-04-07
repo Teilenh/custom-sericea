f1a2cc4b3730ffe54530b21153d6bf772f0510a1
Teilenh <125691903+Teilenh@users.noreply.github.com>
2026-04-07 11:37:20 +0200

    Update LACT download link for Fedora 43

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # this installs a package from fedora repos
-LACT=$(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | grep -oP 'https://github\.com/ilya-zlobintsev/LACT/releases/download/[^"]*lact-headless[^"]*fedora-42\.rpm' | head -n 1)
+LACT=$(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | grep -oP 'https://github\.com/ilya-zlobintsev/LACT/releases/download/[^"]*lact-headless[^"]*fedora-43\.rpm' | head -n 1)
 
 PACKAGES=(
   fastfetch
