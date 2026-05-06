878ab5366c9a6a65fd850ca3318466b686de06c1
Teilenh <125691903+Teilenh@users.noreply.github.com>
2026-05-06 19:12:20 +0200

    Enable and unmask virt sockets for virtualization

--- build_files/systemd-service.sh
+++ build_files/systemd-service.sh
@@ -2,3 +2,9 @@
 set -euo pipefail
 
 systemctl enable podman.socket
+
+for drv in qemu interface network nodedev nwfilter secret storage
+do
+  sudo systemctl unmask virt${drv}d.socket
+  sudo systemctl enable --now virt${drv}d.socket
+done
