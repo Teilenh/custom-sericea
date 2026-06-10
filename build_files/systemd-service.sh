97feb8857a1e212fdea61b8811dfded001911c58
Teilenh <125691903+Teilenh@users.noreply.github.com>
2026-06-10 20:22:06 +0200

    Update systemd-service.sh

--- build_files/systemd-service.sh
+++ build_files/systemd-service.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 systemctl enable podman.socket
 
-for drv in qemu interface network nodedev nwfilter secret storage
-do
-  sudo systemctl unmask virt${drv}d.socket
-  sudo systemctl enable --now virt${drv}d.socket
-done
+#for drv in qemu interface network nodedev nwfilter secret storage
+#do
+# sudo systemctl unmask virt${drv}d.socket
+#  sudo systemctl enable --now virt${drv}d.socket
+#done
