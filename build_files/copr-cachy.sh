3d93bc47166a9164e8abfc01233c66d39f98cf5b
Teilen <125691903+Teilenh@users.noreply.github.com>
2026-08-27 21:25:42 +0200

    Update copr-cachy.sh

--- build_files/copr-cachy.sh
+++ build_files/copr-cachy.sh
@@ -6,6 +6,6 @@ dnf5 copr enable bieszczaders/kernel-cachyos
 dnf5 copr enable bieszczaders/kernel-cachyos-addons
 
 # Install CachyOS kernel
-dnf5 install kernel-cachyos kernel-cachyos-devel
+dnf5 install --setopt=install_weak_deps=False --skip-unavailable -y kernel-cachyos kernel-cachyos-devel
 # enable the above policy to load kernel modules.
 sudo setsebool -P domain_kernel_load_modules on
