b286597a11d26b5199dbbfef326135f6ba4c6d62
Teilen <125691903+Teilenh@users.noreply.github.com>
2026-08-27 19:00:23 +0200

    Update copr-cachy.sh

--- build_files/copr-cachy.sh
+++ build_files/copr-cachy.sh
@@ -1,1 +1,11 @@
+## script for copr of cachy
 
+dnf5 install linux-firmware amd-ucode
+# Add CachyOS COPR repository
+dnf5 copr enable bieszczaders/kernel-cachyos
+dnf5 copr enable bieszczaders/kernel-cachyos-addons
+
+# Install CachyOS kernel
+dnf5 install kernel-cachyos kernel-cachyos-devel
+# enable the above policy to load kernel modules.
+sudo setsebool -P domain_kernel_load_modules on
