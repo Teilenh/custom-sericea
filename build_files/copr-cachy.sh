e4cfdeb831151b70008e811a0c7a57cde2cdfbd4
Teilen <125691903+Teilenh@users.noreply.github.com>
2026-08-28 14:47:38 +0200

    Modify copr-cachy.sh to remove sudo from setsebool
    Removed 'sudo' from the setsebool command to allow it to run without elevated privileges.

--- build_files/copr-cachy.sh
+++ build_files/copr-cachy.sh
@@ -1,6 +1,6 @@
 ## script for copr of cachy
 
-dnf5 install --setopt=install_weak_deps=False --skip-unavailable -y linux-firmware amd-ucode
+dnf5 install --setopt=install_weak_deps=False --skip-unavailable -y linux-firmware
 # Add CachyOS COPR repository
 dnf5 copr enable bieszczaders/kernel-cachyos
 dnf5 copr enable bieszczaders/kernel-cachyos-addons
@@ -8,4 +8,4 @@ dnf5 copr enable bieszczaders/kernel-cachyos-addons
 # Install CachyOS kernel
 dnf5 install --setopt=install_weak_deps=False --skip-unavailable -y kernel-cachyos kernel-cachyos-devel
 # enable the above policy to load kernel modules.
-sudo setsebool -P domain_kernel_load_modules on
+setsebool -P domain_kernel_load_modules on
