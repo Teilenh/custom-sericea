27854826b1070dd8cdf52f07c69129a6cd1b8f7a
Teilen <125691903+Teilenh@users.noreply.github.com>
2026-09-12 14:25:03 +0200

    Update finalize.sh

--- build_files/finalize.sh
+++ build_files/finalize.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -euo pipefail
 
-# Clean DNF cache
 dnf5 config-manager setopt keepcache=0
-
-dnf5 autoremove -y
 dnf5 clean all
 
-# Clean temp files
-rm -rf /tmp/* || true
-find /var/* -maxdepth 0 -type d \
-  ! -name libdnf5 \
-  ! -name rpm-ostree \
-  -exec rm -rf {} + 2>/dev/null || true
+rm -rf \
+    /var/cache/dnf \
+    /var/cache/libdnf5 \
+    /var/log/dnf* \
+    /var/log/hawkey.log \
+    /tmp/*
+
 mkdir -p /var/tmp
-chmod -R 1777 /var/tmp
+chmod 1777 /var/tmp
